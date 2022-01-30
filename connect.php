dc2ac983ea5d8c3256a3b8fb33461a25c1a33a2c
olusoga muyiwa <olusogamuyiwa@gmail.com>
2022-01-30 06:23:59 +0100

    mail not working

--- connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn=new mysqli ($dbserver,$dbuser,$dbpassword);
-if(mysqli_connect_error()){
-        echo ("DATABASE CONNECTION FAILED:" . mysqli_connect_error();
-        else{
-                echo ("DATABASE CONNECTION SUCCEDD.");
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+$name = "feyi";
+echo "$name. <br>";
+echo '<br>.$name';
